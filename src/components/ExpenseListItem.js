6f47b102c351e98d4107fa8a015805baf54c22a7
mrybak834 <mrybak834@gmail.com>
2018-07-17 18:34:19 -0500

    Formatting amount and timestamp

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import moment from 'moment';
+import numeral from 'numeral';
 
 const ExpenseListItem =  ({ id, description, amount, timestamp }) => (
     <div>
         <h3><Link to={`edit/${id}`}>{description}</Link></h3>
-        <p>${amount} - Created At: {timestamp}</p>
+        <p>
+            {numeral(amount / 100).format('$0,0.00')} 
+            - 
+            Created At: {moment(timestamp).format('MMMM Do, YYYY')}
+        </p>
     </div>
 );
 
