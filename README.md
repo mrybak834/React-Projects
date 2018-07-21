7f99002d0fd37114b814259af7a40c67ac059bec
mrybak834 <mrybak834@gmail.com>
2018-07-20 19:11:09 -0500

    Set up firebase dev test and prod servers

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # React-Projects
 Playground for React ideas
+
+
+Notes:
+
+Heroku Environment variable setup:
+    heroku config, displays env vars.
+    heroku config:set KEY=value, to set a var.
+    heroku config:unset KEY, to remove a var.
+
+set firebase vars this way, just copy and paste the 6 vars into config:set.
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -11,12 +11,12 @@ import 'firebase/database';
 
 // Initialize Firebase
 const config = {
-    apiKey: "AIzaSyDhHsLQLIDW3o_vUNXz4zVkcmTd029B6ag",
-    authDomain: "expensify-55e99.firebaseapp.com",
-    databaseURL: "https://expensify-55e99.firebaseio.com",
-    projectId: "expensify-55e99",
-    storageBucket: "expensify-55e99.appspot.com",
-    messagingSenderId: "997376593992"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
 firebase.initializeApp(config);
 
